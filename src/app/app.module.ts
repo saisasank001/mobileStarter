a02dbba3b05989ea4fb12b1e9d1f3abb1a0ceee2
sai <saisasank@autonom8.com>
2019-10-01 17:42:01 +0530

    base ionic project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+/** common module */
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+library.add(fas, far,fab)// add all icons
+
+export const firebaseConfig = {
+  // apiKey: "AIzaSyCo0L3MdkTrx425kUJGtsAg1mt9nUY_P2s",
+  // authDomain: "surveyapp-19da9.firebaseapp.com",
+  // databaseURL: "https://surveyapp-19da9.firebaseio.com",
+  // projectId: "surveyapp-19da9",
+  // storageBucket: "surveyapp-19da9.appspot.com",
+  // messagingSenderId: "157408090936",
+  // appId: "1:157408090936:web:ccd59375fc8b38d7"
+};
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +37,14 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FontAwesomeModule,
+
   ],
   providers: [
     StatusBar,
